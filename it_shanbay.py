74076312aa9360d921ab797dc16f0553743f40e5
alswl <alswlx@gmail.com>
2020-02-13 23:56:19 +0800

    chore: ci

--- it_shanbay.py
+++ it_shanbay.py
@@ -12,10 +12,10 @@ class ITShanbay(unittest.TestCase):
         self.assertFalse(shanbay.is_upgrade_available())
 
     def test_resolve_dns(self):
-        self.assertEqual('1.5.0.0', shanbay._resolve_dns(shanbay.VERSION_DOMAIN))
+        self.assertEqual('1.6.1.0', shanbay._resolve_dns(shanbay.VERSION_DOMAIN))
 
     def test_fetch_version_by_domain(self):
-        self.assertEqual('1.5.0', shanbay._fetch_version_by_domain(shanbay.VERSION_DOMAIN))
+        self.assertEqual('1.6.1', shanbay._fetch_version_by_domain(shanbay.VERSION_DOMAIN))
 
 
 if __name__ == '__main__':
